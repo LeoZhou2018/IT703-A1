8e6ea62f5c34d85a1eeb9041e7c3ab84907d3fba
LeoZhou <zhou.x.l.tiny1986@gmail.com>
2018-08-05 18:06:37 +1200

    Change enter class

--- flaskr/editor.py
+++ flaskr/editor.py
@@ -1,162 +1,11 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
-
+from sqlite3 import Row
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('edit', __name__)
-
-
-def execute_show_company(company=None):
-    contacts = None
-    if company is not None:
-        contacts = get_contact_by_key_value("Company_ID", company["Company_ID"], 1, False)
-    return render_template('edit/customer.html', company=company, contacts=contacts)
-
-
-@bp.route('/edit/show_company')
-@login_required
-def show_company():
-    return execute_show_company()
-
-
-@bp.route('/edit/search_company', methods=('GET', 'POST'))
-@login_required
-def search_company():
-    company = None
-    error = None
-    if request.method == 'POST':
-        company_name = request.form['search_name']
-        if not company_name:
-            error = 'Company name is required.'
-        else:
-            company = get_company_by_key_value("Company_Name", company_name, 1)
-            if company is None:
-                error = "Company {0} doesn't exist.".format(company_name)
-    if error:
-        flash(error)
-    return execute_show_company(company)
-
-
-@bp.route('/edit/edit_company', methods=('GET', 'POST'))
-@login_required
-def edit_company():
-    if request.method == 'POST':
-        error = None
-        form = request.form
-        action = form['action']
-        if action == 'Save':
-            error = add_company(form['company'],
-                                form['location_country'], form['location_city'],
-                                form['location_street'], form['location_post_code'],
-                                form['postal_country'], form['postal_city'],
-                                form['postal_street'], form['postal_post_code'])
-        elif action == 'Update':
-            error = update_company(form['company_id'], form['company'],
-                                   form['location_country'], form['location_city'],
-                                   form['location_street'], form['location_post_code'],
-                                   form['postal_country'], form['postal_city'],
-                                   form['postal_street'], form['postal_post_code'])
-        elif action == 'Remove':
-            error = remove_company(form['company_id'])
-        elif action == 'Add Contact':
-            company_id = form['company_id']
-            if not company_id:
-                error = 'Please search the company name you want to add contact first.'
-            else:
-                company = get_company_by_key_value("Company_ID", company_id, 1)
-                if company:
-                    return execute_show_contact(None, company)
-                else:
-                    error = "The company you want to add contact doesn't exists."
-        flash(error)
-    return render_template('edit/customer.html')
-
-
-def add_company(company_name,
-                location_country, location_city, location_street, location_post_code,
-                postal_country, postal_city, postal_street, postal_post_code):
-    if not company_name:
-        message = 'Please enter the company name you want to save.'
-    else:
-        company = get_company_by_key_value("Company_Name", company_name)
-        if company is not None and company["State"] == 1:
-            message = 'The company you want to save {0} already exists.'.format(company_name)
-        else:
-            execute_add_company(company, company_name,
-                                location_country, location_city, location_street, location_post_code,
-                                postal_country, postal_city, postal_street, postal_post_code)
-            message = "Add company {0} completed.".format(company_name)
-    return message
-
-
-def execute_add_company(company, company_name,
-                        location_country, location_city, location_street, location_post_code,
-                        postal_country, postal_city, postal_street, postal_post_code):
-    if company is None:
-        db = get_db()
-        db.execute(
-            'INSERT INTO customer ('
-            'Company_Name, '
-            'Location_Country, Location_City, Location_Street, Location_Post_Code, '
-            'Postal_Country, Postal_City, Postal_Street, Postal_Post_Code)'
-            ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            (company_name,
-             location_country, location_city, location_street, location_post_code,
-             postal_country, postal_city, postal_street, postal_post_code))
-        company_id = db.lastrowid
-        db.commit()
-    else:
-        execute_update_company(company['Company_ID'], company_name,
-                               location_country, location_city, location_street, location_post_code,
-                               postal_country, postal_city, postal_street, postal_post_code)
-        company_id = company['Company_ID']
-    return company_id
-
-
-def remove_company(company_id):
-    if not company_id:
-        error = 'Please search the company name you want to remove first.'
-    else:
-        db = get_db()
-        company = db.execute(
-            'SELECT * FROM customer WHERE Company_ID = ?', (company_id,)
-        ).fetchall()
-        if len(company) == 0:
-            error = "The company you want to remove doesn't exists."
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE customer SET State = ? WHERE Company_ID = ?',
-                (0, company_id)
-            )
-            db.commit()
-            error = "Remove company {0} completed.".format(company[0]['Company_Name'])
-    return error
-
-
-def update_company(company_id, company_name,
-                   location_country, location_city, location_street, location_post_code,
-                   postal_country, postal_city, postal_street, postal_post_code):
-    if not company_id:
-        error = 'Please search the company name you want to update first.'
-    else:
-        db = get_db()
-        company = db.execute(
-            'SELECT * FROM customer WHERE Company_ID = ?', (company_id,)
-        ).fetchall()
-        if len(company) == 0:
-            error = "The company you want to update doesn't exists."
-        else:
-            if not company_name:
-                error = "Please enter the company name you want to update."
-            else:
-                execute_update_company(company_id, company_name,
-                                       location_country, location_city, location_street, location_post_code,
-                                       postal_country, postal_city, postal_street, postal_post_code)
-                error = "Update company {0} completed.".format(request.form['company'])
-    return error
 
 
 def get_company_by_key_value(key, value, state=None):
@@ -178,10 +27,12 @@ def get_company_by_key_value(key, value, state=None):
     return result[0]
 
 
-def execute_update_company(company_id, company_name,
+def execute_update_company(result, company_id, company_name,
                            location_country, location_city, location_street, location_post_code,
                            postal_country, postal_city, postal_street, postal_post_code):
 
+    if not result:
+        result = dict()
     db = get_db()
     db.execute(
         'UPDATE customer SET '
@@ -196,16 +47,174 @@ def execute_update_company(company_id, company_name,
          1, company_id)
     )
     db.commit()
+    company = dict()
+    company["Company_Name"] = company_name
+    company["Company_ID"] = company_id
+    company["Location_Country"] = location_country
+    company["Location_City"] = location_city
+    company["Location_Street"] = location_street
+    company["Location_Post_Code"] = location_post_code
+    company["Postal_Country"] = postal_country
+    company["Postal_City"] = postal_city
+    company["Postal_Street"] = postal_street
+    company["Postal_Post_Code"] = postal_post_code
+    result['company'] = company
+    return result
 
 
-def get_contacts_by_company_id(company_id):
-    contacts = None
-    if company_id:
+def execute_add_company(result, company, company_name,
+                        location_country, location_city, location_street, location_post_code,
+                        postal_country, postal_city, postal_street, postal_post_code):
+    if not result:
+        result = dict()
+    if company is None:
         db = get_db()
-        contacts = db.execute(
-            'SELECT * FROM contact WHERE Company_ID = ? AND State = ?', (company_id, 1)
-        ).fetchall()
-    return contacts
+        db.execute(
+            'INSERT INTO customer ('
+            'Company_Name, '
+            'Location_Country, Location_City, Location_Street, Location_Post_Code, '
+            'Postal_Country, Postal_City, Postal_Street, Postal_Post_Code)'
+            ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            (company_name,
+             location_country, location_city, location_street, location_post_code,
+             postal_country, postal_city, postal_street, postal_post_code))
+        db.commit()
+        company = get_company_by_key_value("Company_Name", company_name, 1)
+        result['company'] = company
+    else:
+        result = execute_update_company(result, company['Company_ID'], company_name,
+                                        location_country, location_city, location_street, location_post_code,
+                                        postal_country, postal_city, postal_street, postal_post_code)
+    return result
+
+
+def execute_show_company(company=None):
+    contacts = None
+    if company is not None and (isinstance(company, Row) or 'Company_ID' in company):
+        contacts = get_contact_by_key_value("Company_ID", company["Company_ID"], 1, False)
+    db = get_db()
+    titles = db.execute('SELECT * FROM title').fetchall()
+    return render_template('edit/customer.html', company=company, contacts=contacts, titles=titles)
+
+
+@bp.route('/edit/edit_company', methods=('GET', 'POST'))
+@login_required
+def edit_company():
+    result = dict()
+    if request.method == 'POST':
+        form = request.form
+        action = form['action']
+        if action == 'Search':
+            company_name = request.form['search_name']
+            if not company_name:
+                result['message'] = 'Company name is required.'
+            else:
+                company = get_company_by_key_value("Company_Name", company_name, 1)
+                if company is None:
+                    result['message'] = "Company {0} doesn't exist.".format(company_name)
+                else:
+                    result['company'] = company
+        elif action == 'Save':
+            result = add_company(result, form['company'],
+                                 form['location_country'], form['location_city'],
+                                 form['location_street'], form['location_post_code'],
+                                 form['postal_country'], form['postal_city'],
+                                 form['postal_street'], form['postal_post_code'])
+        elif action == 'Update':
+            result = update_company(result, form['company_id'], form['company'],
+                                    form['location_country'], form['location_city'],
+                                    form['location_street'], form['location_post_code'],
+                                    form['postal_country'], form['postal_city'],
+                                    form['postal_street'], form['postal_post_code'])
+        elif action == 'Remove':
+            result = remove_company(result, form['company_id'])
+        elif action == 'Add Contact':
+            company_id = form['company_id']
+            if not company_id:
+                result['message'] = 'Please search the company name you want to add contact first.'
+            else:
+                company = get_company_by_key_value("Company_ID", company_id, 1)
+                if company:
+                    return execute_show_contact(None, company)
+                else:
+                    result['message'] = "The company you want to add contact doesn't exists."
+    if 'message' in result:
+        flash(result['message'])
+    if 'company' in result:
+        return execute_show_company(result['company'])
+    return execute_show_company()
+
+
+def add_company(result, company_name,
+                location_country, location_city, location_street, location_post_code,
+                postal_country, postal_city, postal_street, postal_post_code):
+    if not result:
+        result = dict()
+    if not company_name:
+        result['message'] = 'Please enter the company name you want to save.'
+    else:
+        company = get_company_by_key_value("Company_Name", company_name)
+        if company is not None and company["State"] == 1:
+            result['message'] = 'The company you want to save {0} already exists.'.format(company_name)
+            new_company = dict()
+            new_company["Company_Name"] = company_name
+            new_company["Location_Country"] = location_country
+            new_company["Location_City"] = location_city
+            new_company["Location_Street"] = location_street
+            new_company["Location_Post_Code"] = location_post_code
+            new_company["Postal_Country"] = postal_country
+            new_company["Postal_City"] = postal_city
+            new_company["Postal_Street"] = postal_street
+            new_company["Postal_Post_Code"] = postal_post_code
+            result['company'] = new_company
+        else:
+            result = execute_add_company(result, company, company_name,
+                                         location_country, location_city, location_street, location_post_code,
+                                         postal_country, postal_city, postal_street, postal_post_code)
+            result['message'] = "Add company {0} completed.".format(company_name)
+    return result
+
+
+def remove_company(result, company_id):
+    if not result:
+        result = dict()
+    if not company_id:
+        result['message'] = 'Please search the company name you want to remove first.'
+    else:
+        company = get_company_by_key_value("Company_ID", company_id, 1)
+        if company is None:
+            result['message'] = "The company you want to remove doesn't exists."
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE customer SET State = ? WHERE Company_ID = ?',
+                (0, company_id)
+            )
+            db.commit()
+            result['message'] = "Remove company {0} completed.".format(company['Company_Name'])
+    return result
+
+
+def update_company(result, company_id, company_name,
+                   location_country, location_city, location_street, location_post_code,
+                   postal_country, postal_city, postal_street, postal_post_code):
+    if not result:
+        result = dict()
+    if not company_id:
+        result['message'] = 'Please search the company name you want to update first.'
+    else:
+        company = get_company_by_key_value("Company_ID", company_id)
+        if company is None:
+            result['message'] = "The company you want to update doesn't exists."
+        else:
+            if not company_name:
+                result['message'] = "Please enter the company name you want to update."
+            else:
+                result = execute_update_company(result, company_id, company_name,
+                                                location_country, location_city, location_street, location_post_code,
+                                                postal_country, postal_city, postal_street, postal_post_code)
+                result['message'] = "Update company " + company_name + " completed."
+    return result
 
 
 def get_contact_by_key_value(key, value, state=None, single=True):
@@ -216,11 +225,11 @@ def get_contact_by_key_value(key, value, state=None, single=True):
     db = get_db()
     if state is not None:
         result = db.execute(
-            'SELECT * FROM contact WHERE ? = ? AND State = ?', (key, value, state)
+            'SELECT * FROM contact WHERE ' + key + ' = ? AND State = ?', (value, state)
         ).fetchall()
     else:
         result = db.execute(
-            'SELECT * FROM contact WHERE ? = ?', (key, value)
+            'SELECT * FROM contact WHERE ' + key + ' = ?', (value,)
         ).fetchall()
     if len(result) == 0:
         return None
@@ -235,12 +244,6 @@ def execute_show_contact(contact=None, company=None):
     return render_template('edit/contact.html', titles=titles, contact=contact, company=company)
 
 
-@bp.route('/edit/show_contact')
-@login_required
-def show_contact():
-    return execute_show_contact()
-
-
 @bp.route('/edit/edit_contact', methods=('GET', 'POST'))
 @login_required
 def edit_contact():
@@ -250,8 +253,12 @@ def edit_contact():
         message = None
         form = request.form
         action = form['action']
-        if action == 'New':
-            show_contact()
+        if action == 'Edit':
+            contact_id = form['contact_id']
+            if not contact_id:
+                message = "Contact_ID name is required."
+            else:
+                contact = get_contact_by_key_value("Contact_ID", contact_id, 1)
         elif action == 'Save':
             message = add_contact(form['title'], form['contact_name'], form['company'], form['role'],
                                   form['location_country'], form['location_city'], form['location_street'],
@@ -264,10 +271,11 @@ def edit_contact():
                                      form['phone_cell'], form['phone_home'], form['email'], form['notes'])
         elif action == 'Remove':
             message = remove_contact(form['contact_id'])
-        flash(message)
+        if message is not None:
+            flash(message)
     if contact:
-        company = get_contacts_by_company_id(contact["Company_ID"])
-    show_contact(contact, company)
+        company = get_company_by_key_value("Company_ID", contact["Company_ID"])
+    return execute_show_contact(contact, company)
 
 
 def check_title(title_id):
@@ -307,7 +315,7 @@ def add_contact(title_id, contact_name, company_name, role,
             'Location_Country, Location_City, Location_Street, Location_Post_Code, '
             'Email, Phone_Work, Phone_Cell, Phone_Home, Notes)'
             ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            (title_id, contact_name, company_name, role,
+            (title_id, contact_name, company_id, role,
              location_country, location_city, location_street, location_post_code,
              phone_work, phone_cell, phone_home, email, notes))
         db.commit()
